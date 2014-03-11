963b2fa90435a8e296f78eed115be9c363c55562
Tanner Swenson <TannerSwenson@gmail.com>
2014-03-11 10:12:34 -0700

    Finished lab

--- app/books_controller.js
+++ app/books_controller.js
@@ -5,7 +5,26 @@ bookly.bookApp = angular.module("bookApp", []);
 bookly.BooksController = function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
   $scope.books = books;
-
+  $scope.book_attrs = [
+    {name:'name'},
+    {name:'title'},
+    {name:'price'}
+  ];
+  $scope.cart = [books[2]];
   //Code to manage cart goes here
+  $scope.cartTotal = function () {
+    var i = 0;
+    angular.forEach($scope.cart, function(b, index) {
+      i = i + b.price;
+    });
+    return i;
+  };
 
+  $scope.addBookToCart = function(book) {
+    if ($scope.cart.indexOf(book) > -1 ) {
+
+    } else {
+      $scope.cart.push(book);
+    }
+  };
 };
